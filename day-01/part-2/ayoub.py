a2f374a6e963a2bbfd1944cc3d6e7b8a3de38847
ayoub <ayoub.sbai@student.ecp.fr>
2017-12-01 10:30:37 +0100

    Added day-01 solution

---
+++ day-01/part-2/ayoub.py
@@ -0,0 +1,13 @@
+from submission import Submission
+
+
+class AyoubSubmission(Submission):
+
+	def run(self, s):
+		s = list(map(int, list(s)))
+		n = len(s)
+		r = 0
+		for i in range(n):
+			if s[i] == s[(i+ (n // 2)) % n]:
+				r += s[i]
+		return r
