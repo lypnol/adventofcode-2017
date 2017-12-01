18c3614c4c452c30b710e892f255f24289101e5d
David Dos Santos <david.dossantos@datadoghq.com>
2017-12-01 08:52:14 +0100

    david - day1

---
+++ day-01/part-1/david.py
@@ -0,0 +1,14 @@
+from submission import Submission
+
+
+class DavidSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		# your solution code goes here
+
+
+		return str(sum(int(s[i]) for i in range(len(s)) if s[i-1] == s[i]))
+
+
---
+++ day-01/part-2/david.py
@@ -0,0 +1,13 @@
+from submission import Submission
+
+
+class DavidSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		# your solution code goes here
+		n = len(s)
+
+		return sum(int(s[i]) for i in range(n) if s[i] == s[(i+n//2)%n])
+
