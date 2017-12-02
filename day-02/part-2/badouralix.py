362afce264d6a6feaa02edcb58a8b2c99cdfd090
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2017-12-02 14:31:31 +0100

    [badouralix] day 02

---
+++ day-02/part-2/badouralix.py
@@ -0,0 +1,26 @@
+from submission import Submission
+
+
+class BadouralixSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		rows = s.split('\n')
+		checksum = 0
+
+		for row in rows:
+			cells = [int(n) for n in row.split('\t')]
+			for i in range(len(cells)):
+				for j in range(i+1, len(cells)):
+					maxi = max(cells[i], cells[j])
+					mini = min(cells[i], cells[j])
+					if maxi % mini == 0:
+						checksum += maxi // mini
+						break
+				else:
+					continue
+				break
+
+		return checksum
+
