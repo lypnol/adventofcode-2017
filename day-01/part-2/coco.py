6cd54ad391e1af3a60b5e53e1a268cef3e55526c
Corentin <corentin.dancette@student.ecp.fr>
2017-12-02 15:31:37 +0100

    add coco day 1

---
+++ day-01/part-2/coco.py
@@ -0,0 +1,11 @@
+from submission import Submission
+
+
+class CocoSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		# your solution code goes here
+		return sum([int(s[i]) for i in range(len(s)) if s[i] == s[int(i+len(s)/2) % len(s)]])
+
