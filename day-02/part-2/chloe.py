8e197c93fe22aa630baaf53707e033f56ed53951
ChloeGobe <chloe.gobe@student.ecp.fr>
2017-12-02 10:23:53 +0100

    Chloe Day 2

---
+++ day-02/part-2/chloe.py
@@ -0,0 +1,21 @@
+from submission import Submission
+
+
+class ChloeSubmission(Submission):
+
+	def run(self, s):
+		table_str = [row.split('\t') for row in s.split('\n')]
+		table_int = [list(map(int, row)) for row in table_str]
+
+		total_sum = 0
+		for row in table_int:
+			cell = 0
+			divider = 0
+			while row[cell] % row[divider] != 0 or row[cell] == row[divider] :
+				divider += 1
+				if divider == len(row):
+					divider = 0
+					cell += 1
+			total_sum += row[cell] // row[divider]
+
+		return total_sum
