04ce7a25c8d389f237cf76b59b692bba12da8e8f
ayoub <ayoub.sbai@student.ecp.fr>
2017-12-02 23:20:17 +0100

    Debug mode default & fix mathieu day-02/part-2

--- day-02/part-2/mathieu.py
+++ day-02/part-2/mathieu.py
@@ -6,10 +6,10 @@ class MathieuSubmission(Submission):
 	def run(self, s):
 		inputs=[list(map(int,line.split())) for line in s.split('\n')]
 		res=0
-        for line in inputs:
-            for num1 in line:
-                for num2 in line:
-                    if num1!=num2 and num1%num2==0:
-                        res+=num1/num2
-        return res
+		for line in inputs:
+			for num1 in line:
+				for num2 in line:
+					if num1!=num2 and num1%num2==0:
+						res+=num1/num2
+		return int(res)
 
