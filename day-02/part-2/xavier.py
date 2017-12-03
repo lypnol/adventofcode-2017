2467c799b739068313c5c9f6b83ee4643c24baa0
Xavier Rettel <xavier.rettel@student.ecp.fr>
2017-12-03 22:53:09 +0100

    Fix day 2

--- day-02/part-2/xavier.py
+++ day-02/part-2/xavier.py
@@ -3,14 +3,17 @@ from submission import Submission
 
 class XavierSubmission(Submission):
 
+	def get_dividers(self,r):
+		t = sorted(list(map(int, r.split("\t"))), reverse=True)
+		for i in range(len(t)):
+			for j in range(i + 1, len(t)):
+				if t[i] % t[j] == 0:
+					return [t[i], t[j]]
+
 	def run(self, s):
 		c = 0
-		rows = s.split("\n")
-		for r in rows:
-			t = sorted(list(map(int, r.split("\t"))))
-			m = len(t)
-			for i in range(m):
-				for j in range(i + 1, m):
-					if t[i] % t[j] == 0:
-						c += t[i] - t[j]
+		rs = s.split("\n")
+		for r in rs:
+			t = self.get_dividers(r)
+			c += t[0] // t[1]
 		return c
