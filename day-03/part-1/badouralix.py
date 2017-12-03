5d30bc13cede8b2d8a4a4307fffe6ee0a46f4176
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2017-12-03 15:32:33 +0100

    [badouralix] day 03

---
+++ day-03/part-1/badouralix.py
@@ -0,0 +1,17 @@
+from submission import Submission
+
+
+class BadouralixSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		n = int(s)
+		square = 1
+
+		while (square * square < n):
+			square += 2
+		
+		offset = (square * square - n) % (square - 1)
+		
+		return (square // 2) + abs(square // 2 - offset)
