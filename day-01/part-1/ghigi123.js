cc1e5dd6c10f1d84a7cd2d2756953046bb944071
Ghislain Jeanneau <ghislain@messagera.com>
2017-12-03 22:38:52 +0100

    ghigi123 d1p1

---
+++ day-01/part-1/ghigi123.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+function run(s) {
+    return _.sum(
+        _.map(s,
+            (e, i) =>
+                parseInt(e) === parseInt(s[i === s.length - 1 ? 0 : i + 1])
+                    ? parseInt(e)
+                    : 0)
+    );
+}
