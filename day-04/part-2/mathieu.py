688372b2b96d812dfdda662abbc0d86770769670
Seris <mathieu.seris@student.ecp.fr>
2017-12-04 09:50:51 +0100

    day 4 .1

--- day-04/part-2/mathieu.py
+++ day-04/part-2/mathieu.py
@@ -9,7 +9,7 @@ class MathieuSubmission(Submission):
 		for line in inputs:
 			valid = True
 			for word in line:
-				if line.count(word) > 1 or list(map(set,line)).count(set(word))>1:
+				if list(map(set,line)).count(set(word))>1:
 					valid = False
 			if valid:
 				res += 1
