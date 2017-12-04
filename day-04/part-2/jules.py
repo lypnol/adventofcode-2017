7030097ce8bea3c3993b3fdc9037b55380d700aa
JulesDT <jules.denardou@datadoghq.com>
2017-12-05 00:10:18 +0100

    day-04 jules

---
+++ day-04/part-2/jules.py
@@ -0,0 +1,15 @@
+from submission import Submission
+
+
+class JulesSubmission(Submission):
+
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        # your solution code goes here
+        count = 0
+        for line in s.split('\n'):
+            words = [''.join(sorted(a)) for a in line.split()]
+            unique_words = set(words)
+            count = count + 1 if len(words) == len(unique_words) else count
+        return count
