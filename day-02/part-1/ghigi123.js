2c91525c030c3d3994eeed492b306b4fcf1b26e8
Ghislain Jeanneau <ghislain@messagera.com>
2017-12-04 23:04:56 +0100

    fix jules denardou bullshit

--- day-02/part-1/ghigi123.js
+++ day-02/part-1/ghigi123.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 function run(s) {
     return _.sum(
         _.map(s.split('\n'), line => {
-            const t = _.map(_.split(line, '\t'), a => parseInt(a));
+            const t = _.map(_.split(line, /[\s\t]+/), a => parseInt(a));
             return _.max(t) - _.min(t);
         })
     );
