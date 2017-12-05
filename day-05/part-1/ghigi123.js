35c50be85770d674aa1049d9e06ac73b34698fdf
Ghislain Jeanneau <ghislain@messagera.com>
2017-12-05 09:05:34 +0100

    ghigi123 fix d5p1

--- day-05/part-1/ghigi123.js
+++ day-05/part-1/ghigi123.js
@@ -1,12 +1,5 @@
 const _ = require('lodash');
 
-function rec(instructions, pos) {
-    if(pos >= _.size(instructions) || pos < 0)
-        return 0;
-    instructions[pos]++;
-    return 1 + rec(instructions, pos + instructions[pos] - 1);
-}
-
 run = s => {
     const instructions = _.map(s.split('\n'), i => parseInt(i));
     let pos = 0;
