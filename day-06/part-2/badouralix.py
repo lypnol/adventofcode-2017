7a9059c76e6576fb2851311cb9bd9504e926a809
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2017-12-06 14:49:54 +0100

    [badouralix] day 06

---
+++ day-06/part-2/badouralix.py
@@ -0,0 +1,26 @@
+from submission import Submission
+
+class BadouralixSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		banks = tuple(int(x) for x in s.split())
+		history = dict()
+		while not self.already_seen(banks, history):
+			history[banks] = len(history)
+			banks = self.run_cycle(banks)
+		return len(history) - history[banks]
+
+	def run_cycle(self, banks):
+		banks = list(banks)
+		size = len(banks)
+		max_value = max(banks)
+		max_index = banks.index(max_value)
+		banks[max_index] = 0
+		for offset in range(1, size+1):
+			banks[(max_index + offset) % size] += (max_value // size) + (offset <= max_value % size)
+		return tuple(banks)
+
+	def already_seen(self, banks, history):
+		return banks in history
