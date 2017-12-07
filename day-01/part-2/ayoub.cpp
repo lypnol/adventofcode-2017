a80b236384546430bf0bb04a41e45c1926cf7f11
ayoub <ayoub.sbai@student.ecp.fr>
2017-12-07 17:22:48 +0100

    Added day-01 solution c++

---
+++ day-01/part-2/ayoub.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string run(string s) {
+	int sum = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == s[(i + s.size() / 2) % s.size()]) {
+            sum += s[i] - '0';
+        }
+    }
+    return to_string(sum);
+}
+
+int main(int argc, char** argv) {
+    cout << run(string(argv[1])) << "\n";
+    return 0;
+}
