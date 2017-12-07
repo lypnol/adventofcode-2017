649b67b803ef4eef24b2227221884e545c2128c8
Seris <mathieu.seris@student.ecp.fr>
2017-12-07 20:56:53 +0100

    debug d7p2

--- day-07/part-2/mathieu.py
+++ day-07/part-2/mathieu.py
@@ -8,26 +8,9 @@ class MathieuSubmission(Submission):
         total_weights = dict()
         for program in weights:
             total_weights[program] = self.total_weights(program, weights, sons)
-        wrong_programs=self.wrong_weight(total_weights, weights, sons)
-
-        return min(wrong_programs, key=lambda x :x[0])[1]
-
-    def wrong_weight(self, total_weights, weights, sons, ):
-        wrong_programs=[]
-        for program in sons:
-            total_weights_son = [(subprogram, total_weights[subprogram]) for subprogram in sons[program]]
-            sub_max, max_son = max(total_weights_son, key=lambda x: x[1])
-            sub_min, min_son = min(total_weights_son, key=lambda x: x[1])
-            if max_son - min_son:
-                wrong_programs.append((sub_max,weights[sub_max]-max_son+min_son))
-        return wrong_programs
-
-    def total_weights(self, program, weights, sons):
-        res = 0
-        if program in sons:
-            for sub_program in sons[program]:
-                res += self.total_weights(sub_program, weights, sons)
-        return weights[program] + res
+        wrong_programs = self.wrong_weight(total_weights, weights, sons)
+        print(wrong_programs)
+        return min(wrong_programs, key=lambda x: x[0])[1]
 
     def extract(self, inputs):
         weights = dict()
@@ -41,3 +24,20 @@ class MathieuSubmission(Submission):
                     sons[line[0]].add(son)
                     children.add(son)
         return weights, sons, (set(weights.keys()) - set(children)).pop()
+
+    def total_weights(self, program, weights, sons):
+        res = 0
+        if program in sons:
+            for sub_program in sons[program]:
+                res += self.total_weights(sub_program, weights, sons)
+        return weights[program] + res
+
+    def wrong_weight(self, total_weights, weights, sons):
+        wrong_programs = []
+        for program in sons:
+            total_weights_son = [(subprogram, total_weights[subprogram]) for subprogram in sons[program]]
+            sub_max, max_son = max(total_weights_son, key=lambda x: x[1])
+            sub_min, min_son = min(total_weights_son, key=lambda x: x[1])
+            if max_son - min_son:
+                wrong_programs.append((max_son, weights[sub_max] - max_son + min_son))
+        return wrong_programs
