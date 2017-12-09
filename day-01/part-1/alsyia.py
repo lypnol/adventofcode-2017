00b2b5adf35387771376621a934f52e0983a8f9c
Danguy Des Deserts Venceslas <wenceslas.desdeserts@laposte.net>
2017-12-09 22:28:21 +0100

    [alsyia] Day 01

---
+++ day-01/part-1/alsyia.py
@@ -0,0 +1,9 @@
+from runners.python import Submission
+
+class AlsyiaSubmission(Submission):
+
+	def run(self, s):
+		# :param s: input in string format
+		# :return: solution flag
+		return sum([int(s[i]) for i in range(len(s)) if s[i-1] == s[i]])
+
