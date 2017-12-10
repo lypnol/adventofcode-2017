e48d6e061ed06d7414c196ba3756a3fffb21442c
Xavier Rettel <xavier.rettel@student.ecp.fr>
2017-12-10 19:38:43 +0100

    Xavier Day 8

---
+++ day-08/part-1/xavier.py
@@ -0,0 +1,20 @@
+from runners.python import Submission
+from collections import defaultdict
+
+class XavierSubmission(Submission):
+	def inc_or_dec(self, w, v):
+		if w == "inc":
+			return int(v)
+		else:
+			return -1 * int(v)
+
+	def run(self, s):
+		s = s.split("\n")
+		registers = defaultdict(lambda: 0)
+		for line in s:
+			line = line.split(" ")
+			comp = line[5]
+			line[6] = int(line[6])
+			if comp == "==" and registers[line[4]] == line[6] or comp == ">=" and registers[line[4]] >= line[6] or comp == "<=" and registers[line[4]] <= line[6] or comp == "!=" and registers[line[4]] != line[6] or comp == "<" and registers[line[4]] < line[6] or comp == ">" and registers[line[4]] > line[6]:
+				registers[line[0]] += self.inc_or_dec(line[1], line[2])
+		return max(registers.values())
