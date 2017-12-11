1fe506a396761e3b1ab26f418ed35f7bd07fb065
Xavier Rettel <xavier.rettel@student.ecp.fr>
2017-12-11 11:29:59 +0100

    Fix day 11

--- day-11/part-1/xavier.py
+++ day-11/part-1/xavier.py
@@ -9,4 +9,4 @@ class XavierSubmission(Submission):
 		v_axis = count["n"] - count["s"]
 		d1_axis = count["sw"] - count["ne"]
 		d2_axis = count["nw"] - count["se"]
-		return (abs(d1_axis - d2_axis) + abs(d2_axis + v_axis) + abs(v_axis + d1_axis)) / 2
+		return (abs(d1_axis + d2_axis) + abs(d2_axis + v_axis) + abs(-v_axis + d1_axis)) // 2
