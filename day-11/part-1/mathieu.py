a25c95d3179418c1110110f86008bb413a5ba867
Seris <mathieu.seris@student.ecp.fr>
2017-12-11 11:03:50 +0100

    day11.1

--- day-11/part-1/mathieu.py
+++ day-11/part-1/mathieu.py
@@ -3,15 +3,7 @@ from runners.python import Submission
 
 class MathieuSubmission(Submission):
     def run(self, s):
-        n_pos = 0
-        e_pos = 0
-        for char in s:
-            if char == "e":
-                e_pos += 1
-            elif char == "w":
-                e_pos -= 1
-            elif char == "n":
-                n_pos += 1
-            elif char == "s":
-                n_pos -= 1
+        # Inspiré de la façon de "compter" de Xavier
+        n_pos = s.count('n')-s.count('s')
+        e_pos = s.count('e')-s.count('w')
         return max(abs(n_pos),abs(e_pos))
