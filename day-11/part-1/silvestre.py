d3b09e022b7d789403f66f1828ac8978c10fa4ac
Silvestre Perret <silvestre.perret@gmail.com>
2017-12-11 11:06:59 +0100

    silvestre-d11-fix

--- day-11/part-1/silvestre.py
+++ day-11/part-1/silvestre.py
@@ -7,7 +7,6 @@ class SilvestreSubmission(Submission):
         clairement inspiré de xavier, j'ai pris un autre référentiel et une autre formule pour la forme
         """
         instructions = s.split(",")
-        n_axis = instructions.count("n") - instructions.count("s")
-        ne_axis = instructions.count("ne") - instructions.count("sw")
-
-        return max(abs(n_axis), abs(ne_axis), abs(-n_axis-ne_axis))
+        s_axis = instructions.count("s") - instructions.count("n") + instructions.count("sw") - instructions.count("ne") 
+        se_axis = instructions.count("se") - instructions.count("nw") + instructions.count("ne") - instructions.count("sw")
+        return max(abs(s_axis), abs(se_axis), abs(-s_axis-se_axis))
--- day-11/part-2/silvestre.py
+++ day-11/part-2/silvestre.py
@@ -8,17 +8,23 @@ class SilvestreSubmission(Submission):
         """
         instructions = s.split(",")
         max_distance = 0
-        n_axis = 0
-        ne_axis = 0
+        s_axis = 0
+        se_axis = 0
         for instruction in instructions:
-            if instruction == "n":
-                n_axis += 1
             if instruction == "s":
-                n_axis -= 1
+                s_axis += 1
+            if instruction == "n":
+                s_axis -= 1
+            if instruction == "se":
+                se_axis += 1
+            if instruction == "nw":
+                se_axis -= 1
             if instruction == "ne":
-                ne_axis += 1
+                s_axis -= 1
+                se_axis += 1
             if instruction == "sw":
-                ne_axis -= 1
-            max_distance = max(max_distance, max(abs(n_axis), abs(ne_axis), abs(-n_axis-ne_axis)))
+                s_axis += 1
+                se_axis -= 1
+            max_distance = max(max_distance, max(abs(s_axis), abs(se_axis), abs(-s_axis-se_axis)))
 
         return max_distance
