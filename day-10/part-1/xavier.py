d9485840ccb58d2a19a8b8e3fae7c3715714e388
Xavier Rettel <xavier.rettel@student.ecp.fr>
2017-12-11 08:52:06 +0100

    Code cleanup

--- day-10/part-1/xavier.py
+++ day-10/part-1/xavier.py
@@ -1,19 +1,18 @@
 from runners.python import Submission
 
-class XavierSubmission(Submission):
 
+class XavierSubmission(Submission):
 	def run(self, s):
 		s = list(map(int, s.split(",")))
 		c_list = list(range(256))
 		cur_pos = 0
 		skip = 0
 		for length in s:
-			buffer = [c_list[(cur_pos+i)%256] for i in range(length)]
+			buffer = [c_list[(cur_pos + i) % 256] for i in range(length)]
 			buffer = buffer[::-1]
 			for i in range(length):
-				c_list[(cur_pos+i)%256] = buffer[i]
+				c_list[(cur_pos + i) % 256] = buffer[i]
 			cur_pos += length + skip
 			cur_pos = cur_pos % 256
 			skip += 1
 		return c_list[0] * c_list[1]
-		
