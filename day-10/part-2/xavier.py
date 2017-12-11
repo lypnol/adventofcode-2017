f83e3f433c22796174817e6e18dae6d386cabc35
Xavier Rettel <xavier.rettel@student.ecp.fr>
2017-12-11 08:51:13 +0100

    Day 10 part 2

--- day-10/part-2/xavier.py
+++ day-10/part-2/xavier.py
@@ -1,19 +1,22 @@
 from runners.python import Submission
 
+
 class XavierSubmission(Submission):
 
 	def run(self, s):
-		s = list(map(int, s.split(",")))
+		s = list(map(ord, s)) + [17, 31, 73, 47, 23]
 		c_list = list(range(256))
 		cur_pos = 0
 		skip = 0
-		for length in s:
+		for length in s*64:
 			buffer = [c_list[(cur_pos+i)%256] for i in range(length)]
 			buffer = buffer[::-1]
 			for i in range(length):
 				c_list[(cur_pos+i)%256] = buffer[i]
-			cur_pos += length + skip
-			cur_pos = cur_pos % 256
+			cur_pos = (cur_pos + length + skip) % 256
 			skip += 1
-		return c_list[0] * c_list[1]
-		
+		hashes = [c_list[16*i] for i in range(16)]
+		for i in range(16):
+			for j in range(1,16):
+				hashes[i] ^= c_list[i*16+j]
+		return ''.join(map(lambda x: hex(x)[2:].zfill(2), hashes))
