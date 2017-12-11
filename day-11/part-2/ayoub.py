286bdefa9f7d51a577a317ce426050428cec689c
ayoub <ayoub.sbai@student.ecp.fr>
2017-12-11 11:45:06 +0100

    fixed p2 solution

--- day-11/part-2/ayoub.py
+++ day-11/part-2/ayoub.py
@@ -13,50 +13,14 @@ class AyoubSubmission(Submission):
 		'se': (0, 1)
 	}
 
-	@staticmethod
-	def manhattan(a, b): 
-		return abs(a[0]-b[0])+abs(a[1]-b[1])
-
-	def a_star(self, start, end, heur):
-		dist = {start: 0}
-		visited = set()
-		Q = hq.Fibonacci_heap()
-		Q_index = {}
-		Q_index[start] = Q.enqueue(start, 0)
-		while Q:
-			current = Q.dequeue_min().get_value()
-			visited.add(current)
-
-			if heur(current, end) < 0.1:
-				return dist[current]
-
-			for child in self.get_children(current):
-				if child in visited: 
-					continue
-				if child not in dist:
-					dist[child] = dist[current] + 1
-					Q_index[child] = Q.enqueue(child, dist[child] + heur(child, end))
-				elif child in dist and dist[child] > dist[current] + 1:
-					dist[child] = dist[current] + 1
-					Q.decrease_key(Q_index[child], dist[child] + heur(child, end))
-
-	def get_children(self, node):
-		x, y = node
-		children = []
-		for u, v in self.direction.values():
-			children.append((x+u, y+v))
-		return children
-
+	# Using https://www.redblobgames.com/grids/hexagons/#coordinates-cube
 	def run(self, s):
-		start = (0, 0)
-		end = (0, 0)
+		q, r = 0, 0
 		max_dist = 0
-		furthest = (0, 0)
 		for move in s.split(','):
 			u, v = self.direction[move]
-			end = (end[0]+u, end[1]+v)
-			if max_dist < self.manhattan(start, end):
-				max_dist = self.manhattan(start, end)
-				furthest = end
-		
-		return self.a_star(start, furthest, self.manhattan)
+			q, r = q+u, r+v
+			x, y, z = q, r, -q-r
+			dist = (abs(x) + abs(y) + abs(z)) // 2
+			max_dist = max(max_dist, dist)
+		return max_dist
