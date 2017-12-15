4381a608dbfca11468ebc0bde6105414b93e40a8
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2017-12-16 00:31:59 +0100

    [badouralix] reimplementing mod 0x7fffffff operation

--- day-15/part-1/badouralix.c
+++ day-15/part-1/badouralix.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define FACTOR_A 16807
-#define FACTOR_B 48271
+#define FACTOR_A 0x41a7
+#define FACTOR_B 0xbc8f
 #define LIMIT 40000000
 
 const int run(char* s)
 {
-    long a, b = atoi(strrchr(s, ' ') + 1);
-    int result = 0;
-    const char newline[] = "\n",
-               *token = strtok(s, newline);
+    int64_t a, b = atoi(strrchr(s, ' ') + 1),
+            result = 0;
+    const char *token = strtok(s, "\n");
     token = '\0';
     a = atoi(strrchr(s, ' ') + 1);
 
     for (int i = 0; i < LIMIT; i++) {
-        a = (a * FACTOR_A) % 0x7fffffff;
-        b = (b * FACTOR_B) % 0x7fffffff;
+        a = ((a * FACTOR_A) & 0x7fffffff) + ((a * FACTOR_A) >> 31);
+        a -= (a > 0x7fffffff) * 0x7fffffff;
 
-        result += ((a ^ b) & 0xffff) == 0;
+        b = ((b * FACTOR_B) & 0x7fffffff) + ((b * FACTOR_B) >> 31);
+        b -= (b > 0x7fffffff) * 0x7fffffff;
+
+        result += ((a ^ b) & 0xffff) == 0x0000;
     }
 
     return result;
--- day-15/part-2/badouralix.c
+++ day-15/part-2/badouralix.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define FACTOR_A 16807
-#define FACTOR_B 48271
+#define FACTOR_A 0x41a7
+#define FACTOR_B 0xbc8f
 #define LIMIT 5000000
 
 const int run(char* s)
 {
-    long a, b = atoi(strrchr(s, ' ') + 1);
-    int result = 0;
-    const char newline[] = "\n",
-               *token = strtok(s, newline);
+    int64_t a, b = atoi(strrchr(s, ' ') + 1),
+            result = 0;
+    const char *token = strtok(s, "\n");
     token = '\0';
     a = atoi(strrchr(s, ' ') + 1);
 
+
     for (int i = 0; i < LIMIT; i++) {
         do {
-            a = (a * FACTOR_A) % 0x7fffffff;
-        } while ((a & 0b11) != 0);
+            a = ((a * FACTOR_A) & 0x7fffffff) + ((a * FACTOR_A) >> 31);
+            a -= (a > 0x7fffffff) * 0x7fffffff;
+        } while ((a & 0b11) != 0b00);
 
         do {
-            b = (b * FACTOR_B) % 0x7fffffff;
-        } while ((b & 0b111) != 0);
+            b = ((b * FACTOR_B) & 0x7fffffff) + ((b * FACTOR_B) >> 31);
+            b -= (b > 0x7fffffff) * 0x7fffffff;
+        } while ((b & 0b111) != 0b000);
 
-        result += ((a ^ b) & 0xffff) == 0;
+        result += ((a ^ b) & 0xffff) == 0x0000;
     }
 
     return result;
