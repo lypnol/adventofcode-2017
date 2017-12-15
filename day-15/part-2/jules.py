8c335ff52d2d54f0bfd795f8a1d38c6b82cd2486
JulesDT <jules.denardou@datadoghq.com>
2017-12-15 10:43:57 +0100

    fix gen

---
+++ day-15/part-2/jules.py
@@ -0,0 +1,23 @@
+from runners.python import Submission
+
+
+class JulesSubmission(Submission):
+
+    def run(self, s):
+        lines = s.split('\n')
+        genA, genB, n = 16807, 48271, 2147483647
+        a = int(lines[0].split()[-1])
+        b = int(lines[1].split()[-1])
+        i = 0
+        score = 0
+        while i < 5000000:
+            i += 1
+            a = (a * genA) % n
+            while a % 4 != 0:
+                a = (a * genA) % n
+            b = (b * genB) % n
+            while b % 8 != 0:
+                b = (b * genB) % n
+            if (a & 0xffff) == (b & 0xffff):
+                score += 1
+        return score
