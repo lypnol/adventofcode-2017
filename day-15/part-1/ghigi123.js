9ba2e63598e728b713831fca1ef9d656179192ab
Ghislain Jeanneau <ghislain@messagera.com>
2017-12-15 10:21:58 +0100

    ghigi123 move part1 to part2 ...

--- day-15/part-1/ghigi123.js
+++ day-15/part-1/ghigi123.js
@@ -9,9 +9,8 @@ const Mod2 = 65535;
 const run = s => {
 	let [, a, b] = /(\d+)\n.*?(\d+)/g.exec(s);
 	let A = parseInt(a), B = parseInt(b), N = 0;
-	for (let i = 0; i < 5000000; i++) {
-		do A = (A * GenAMul) % Mod; while(A & 3)
-		do B = (B * GenBMul) % Mod; while(B & 7)
+	for (let i = 0; i < 40000000; i++) {
+		A = (A * GenAMul) % Mod, B = (B * GenBMul) % Mod;
 		N += ((A & Mod2) ^ (B & Mod2)) === 0;
 	}
 	return N;
--- day-15/part-2/ghigi123.js
+++ day-15/part-2/ghigi123.js
@@ -1,7 +1,18 @@
-/**
- * @param {{string}} s puzzle input in string format
- * @returns solution flag
- */
-run = s => {
-	// Your code goes here
+const _ = require('lodash')
+
+const GenAMul = 16807;
+const GenBMul = 48271;
+
+const Mod = 2147483647;
+const Mod2 = 65535;
+
+const run = s => {
+	let [, a, b] = /(\d+)\n.*?(\d+)/g.exec(s);
+	let A = parseInt(a), B = parseInt(b), N = 0;
+	for (let i = 0; i < 5000000; i++) {
+		do A = (A * GenAMul) % Mod; while(A & 3)
+		do B = (B * GenBMul) % Mod; while(B & 7)
+		N += ((A & Mod2) ^ (B & Mod2)) === 0;
+	}
+	return N;
 };
