260b5f5236d9cd733e31a3018b378a13446e3b51
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2017-12-15 14:26:27 +0100

    [badouralix] cing day 15

---
+++ day-15/part-2/badouralix.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FACTOR_A 16807
+#define FACTOR_B 48271
+#define LIMIT 5000000
+
+const int run(char* s)
+{
+    long a, b = atoi(strrchr(s, ' ') + 1);
+    int result = 0;
+    const char newline[] = "\n",
+               *token = strtok(s, newline);
+    token = '\0';
+    a = atoi(strrchr(s, ' ') + 1);
+
+    for (int i = 0; i < LIMIT; i++) {
+        do {
+            a = (a * FACTOR_A) % 0x7fffffff;
+        } while ((a & 0b11) != 0);
+
+        do {
+            b = (b * FACTOR_B) % 0x7fffffff;
+        } while ((b & 0b111) != 0);
+
+        result += ((a ^ b) & 0xffff) == 0;
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    printf("%d\n", run(argv[1]));
+    return 0;
+}
