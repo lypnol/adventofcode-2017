e6a199a62fc99ff335c130d2b3bed10d83f1b080
Seris <mathieu.seris@student.ecp.fr>
2017-12-17 13:13:35 +0100

    day17.1

--- day-17/part-2/mathieu.py
+++ day-17/part-2/mathieu.py
@@ -4,11 +4,10 @@ from runners.python import Submission
 class MathieuSubmission(Submission):
     def run(self, s):
         step = int(s)
-        i = 1
         current_pos = 0
-        while i <= 50000000:
+        for i in range(1,50000001):
             current_pos = (current_pos + step) % i + 1
             if current_pos == 1:
                 ret = i
-            i += 1
         return ret
+
